aa0d90cb52c8ff13f5c5bfa74aabcc8dd60d35e7
Charles Torry <ctorry@gmail.com>
2019-02-25 22:05:33 -0500

    lotaa changes

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,26 +1,28 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: []
 })
 export class NewAccountComponent {
   myCounter = 0;
 
-  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
-
-  constructor(private loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {
+    this.accountsService.statusUpdated.subscribe((status: string) =>
+      alert('New Status:' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
+    this.accountsService.addAccount(accountName, accountStatus);
     // console.log('A server status changed, new status: ' + accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
